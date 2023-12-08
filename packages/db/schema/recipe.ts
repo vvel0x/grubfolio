cca2813027f21a366cdcb46b87d4b8f3407c3030
Issa <vvel0x@users.noreply.github.com>
2023-12-08 03:59:04 +0000

    feat(recipes): add db schema

--- packages/db/schema/recipe.ts
+++ packages/db/schema/recipe.ts
@@ -1,11 +1,49 @@
 import { createId } from "@paralleldrive/cuid2";
-import { pgTable, text, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  numeric,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const recipes = pgTable("recipes", {
   id: varchar("id")
     .primaryKey()
     .$defaultFn(() => createId()),
-  name: varchar("name"),
-  excerpt: text("excerpt"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+  authorId: varchar("author_id").notNull(),
+  name: varchar("name").notNull(),
   source: varchar("source"),
+  img_url: varchar("img_url"),
+  description: text("description"),
+  instructions: text("instructions"),
+  servings: integer("servings"),
+  prep_time: integer("prep_time"),
+  cook_time: integer("cook_time"),
+  notes: text("notes"),
 });
+
+export const ingredients = pgTable("ingredients", {
+  id: varchar("id")
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  quantity: numeric("quantity"),
+  unit: varchar("unit"),
+  name: varchar("name").notNull(),
+  intruction: text("intruction"),
+});
+
+export const recipe_ingredients = pgTable(
+  "recipe_ingredients",
+  {
+    recipe_id: varchar("recipe_id").references(() => recipes.id),
+    ingredient_id: varchar("ingredient_id").references(() => ingredients.id),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.recipe_id, table.ingredient_id] }),
+  }),
+);
